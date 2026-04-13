cd494025221d169e624defdaf4339d1a150c9157
Alexander <pchp250g8@tutamail.com>
2026-04-13 12:34:55 +0300

    Fixed Source Code

--- OctToDec/Program.cs
+++ OctToDec/Program.cs
@@ -12,9 +12,9 @@ namespace OctToDec
         static void Main(string[] args)
         {
             const uint MAX_INT = uint.MaxValue;
-            var nDecNum = 0;
-            var nOctalPower = 1;
-            var nMaxOctalLen = Math.Truncate(Math.Log(MAX_INT, 8));
+            var nDecNum = 0L;
+            var nOctalPower = 1L;
+            var nMaxOctalLen = Math.Ceiling(Math.Log(MAX_INT, 8));
             var oRegEx = new Regex("^[0-7]+$");
             Console.WriteLine("Input a octal number");
             var strLine = Console.ReadLine();
